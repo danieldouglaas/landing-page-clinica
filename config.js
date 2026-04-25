ed0237c715c5c84a4a979613906d2d5031796e88
Daniel Douglas Cruz Lima <daniel.lima@gruposimoes.com.br>
2026-04-25 12:36:26 -0400

    Initial commit

---
+++ config.js
@@ -0,0 +1,36 @@
+const DADOS = {
+    // -----------------------------------------
+    // 1. INFORMAÇÕES GERAIS E CONTATO
+    // -----------------------------------------
+    "nomeClinica": "Dental Dreams",
+    "nomeCroProfissional": "Dra. Danielle Cruz | CRO-AM 8902",
+    "sobreMimTexto": "Sou a Dra. Danielle Cruz (CRO-AM 8902), e acredito que um sorriso saudável começa com um atendimento humanizado. Meu consultório foi pensado para oferecer o máximo de conforto e tranquilidade, garantindo que você e sua família se sintam acolhidos desde o primeiro momento. Conto com grande experiência e tecnologia de ponta para cuidar da sua saúde bucal e transformar o seu sorriso.",
+    "telefone": "(92) 98607-0067", // Seu telefone fixo ou celular
+    "whatsappLink": "https://wa.me/5592986070067", // Link direto do WhatsApp (ex: https://wa.me/55DDDNUMERO)
+    "instagramUsuario": "@dradaniellemenezes_",
+    "instagramLink": "https://www.instagram.com/dradaniellemenezes_",
+    "cidade": "Itapiranga - AM",
+    "endereco": "Rua Antônio Almeida Santos, 7, Centro, Itapiranga - AM, 69120-000",
+    "enderecoLink": "https://www.google.com/maps/search/?api=1&query=R.+Antônio+Almeida+Santos,+7,+Itapiranga+-+AM,+69120-000",
+    "horarioSemana": "Segunda a Sexta: 09:00 às 17:00",
+    "horarioFimDeSemana": "Sábado: 09:00 às 12:00",
+
+    // -----------------------------------------
+    // 2. FOTOS E IMAGENS
+    // Coloque os nomes das fotos que você salvou na pasta 'assets'
+    // -----------------------------------------
+
+    // Se não tiver logotipo ainda, deixe vazio "" que o espaço some automaticamente
+    "logoImagem": "",
+
+    // Imagem grande de fundo do início da página
+    "heroFundoImagem": "assets/hero_dentist_family_1777127315265.png",
+
+    // Sua foto na seção "Sobre Mim"
+    "sobreMimImagem": "assets/foto_perfil.jpg",
+
+    // Fotos do seu espaço/clínica para a Galeria
+    "galeriaImagem1": "assets/clinic_reception_1777127327768.png",
+    "galeriaImagem2": "assets/hero_dentist_family_1777127315265.png",
+    "galeriaImagem3": "assets/clinic_reception_1777127327768.png"
+};
